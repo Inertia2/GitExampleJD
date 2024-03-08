6de63a5343dbf803cbc02b2a96099b5e623eda1e
Josh Dowrick <j.dowrick@tees.ac.uk>
2024-03-08 15:11:26 +0000

    Initial Commit

--- app/src/main/java/com/example/gitexamplejd/MainActivity.kt
+++ app/src/main/java/com/example/gitexamplejd/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("GitCommit3")
+                    Greeting("GitCommit4")
                 }
             }
         }
